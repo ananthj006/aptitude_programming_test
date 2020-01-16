6fffdaf54d834a808edbdd953ccaa08e982d39e6
ananthj006 <ananthj006@gmail.com>
2020-01-16 23:43:11 +0530

    Add files via upload

---
+++ tsMastery.ts
@@ -0,0 +1,32 @@
+class MathClass {
+    @subtract(1)
+    @multiply(2)
+    addOne(number: number) {
+        return number + 1;
+    }
+}
+console.log(new MathClass().addOne(2))
+
+function multiply(value: number) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {        
+        let descriptorFunc = descriptor.value;
+        descriptor.value = function () {
+            let obj = descriptorFunc.apply(target, arguments);
+            return obj * value;
+        };
+
+        return descriptor;
+    };
+}
+
+function subtract(value: number) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        let descriptorFunc = descriptor.value;
+        descriptor.value = function () {
+            let obj = descriptorFunc.apply(target, arguments);
+            return obj - value;
+        };
+
+        return descriptor;
+    };
+}
